627b90a44f3a35ba1b2448fea82395917b15460e
Ілля Шаранов <sharanov.i.m@nmu.one>
2025-03-25 17:45:10 +0200

    minor controller changes

--- src/main/java/com/example/try1/controller/ProductController.java
+++ src/main/java/com/example/try1/controller/ProductController.java
@@ -1,8 +1,9 @@
 package com.example.try1.controller;
 
-import com.example.try1.DatabaseHandler;
+import com.example.try1.model.Product;
 import com.example.try1.service.CurrencyService;
 import com.example.try1.service.ExcelService;
+import com.example.try1.service.ProductParserService;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -14,9 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
 import java.io.IOException;
-import com.example.try1.service.ProductParserService;
-import com.example.try1.model.Product;
-
 import java.util.List;
 
 @RestController
@@ -24,40 +22,30 @@ import java.util.List;
 public class ProductController {
 
     private final ProductParserService parserService;
+    private final CurrencyService currencyService;
+    private final ExcelService excelService;
 
-    public ProductController(ProductParserService parserService) {
+    public ProductController(ProductParserService parserService, CurrencyService currencyService, ExcelService excelService) {
         this.parserService = parserService;
+        this.currencyService = currencyService;
+        this.excelService = excelService;
     }
 
     @GetMapping
     public List<Product> getProducts() {
-        return parserService.getProducts(); // Отримати продукти
+        return parserService.getProducts();
     }
 
     @GetMapping("/download")
     public ResponseEntity<Resource> downloadExcel() throws IOException {
-        // Генеруємо файл і зберігаємо його в тимчасовій папці
-        ProductParserService productParserService = new ProductParserService();
-        List<Product> products = productParserService.getProducts();
-        CurrencyService currencyService = new CurrencyService();
-        ExcelService excelService = new ExcelService();
+        List<Product> products = parserService.getProducts();
         float usdRate = currencyService.getUsdRate();
         File file = excelService.saveProductsToExcel(products, usdRate);
-
-        DatabaseHandler databaseHandler = new DatabaseHandler();
-        databaseHandler.processExcelFile(file); // Обробляємо файл з продуктами і зберігаємо в БД
-
         Resource resource = new FileSystemResource(file);
 
-        // Відправляємо файл на скачування
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=products.xlsx")
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
                 .body(resource);
-    }
-
-    @GetMapping("/")
-    public String index() {
-        return "index"; // Файл index.html в папці templates
     }
 }
